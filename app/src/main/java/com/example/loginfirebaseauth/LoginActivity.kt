30ad138b23236b3c25202a8264c56f94433ab9b6
Matheus Souza <matheusouzaaaa@gmail.com>
2021-06-15 22:21:22 -0300

    Login com o firestore

---
+++ app/src/main/java/com/example/loginfirebaseauth/LoginActivity.kt
@@ -0,0 +1,64 @@
+package com.example.loginfirebaseauth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.example.loginfirebaseauth.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+
+        binding.signInAppCompatButton.setOnClickListener {
+            val mEmail = binding.emailEditText.text.toString()
+            val mPassword = binding.passwordEditText.text.toString()
+
+            when {
+                mEmail.isEmpty() || mPassword.isEmpty() -> {
+                    Toast.makeText(baseContext, "Email ou senha incorretos",Toast.LENGTH_SHORT).show()
+                } else -> {
+                SignIn(mEmail, mPassword)
+            }
+            }
+
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private fun SignIn (email : String , password : String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("TAG", "signInWithEmail:success")
+                    reload()
+                } else {
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Email ou senha incorretos",Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun reload() {
+        val intent = Intent (this, MainActivity::class.java)
+        this.startActivity(intent)
+    }
+}
--- app/src/main/java/com/example/loginfirebaseauth/MainActivity.kt
+++ app/src/main/java/com/example/loginfirebaseauth/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.loginfirebaseauth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.loginfirebaseauth.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+    }
+
+    fun logout(view: View){
+        Firebase.auth.signOut()
+        val intent = Intent (this, LoginActivity::class.java)
+        this.startActivity(intent)
     }
 }
